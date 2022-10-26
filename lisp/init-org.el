d1fd8491ec64c99f49c4e256745e218a948f7af7
Qing Yin <qingbyin@gmail.com>
2022-10-26 14:11:04 +0800

    Modify todo keywords

--- lisp/init-org.el
+++ lisp/init-org.el
@@ -7,16 +7,21 @@
 (setq org-log-done t)
 
 ; Evil mode
+(evil-define-key 'normal org-mode-map (kbd "T")
+                 (lambda () (interactive) (org-todo "TODO")))
+(evil-define-key 'normal org-mode-map (kbd "N")
+                 (lambda () (interactive) (org-todo "NEXT")))
 (evil-define-key 'normal org-mode-map (kbd "D")
                  (lambda () (interactive) (org-todo "DONE")))
-(evil-define-key 'normal org-mode-map (kbd "C")
-                 (lambda () (interactive) (org-todo "CANCELLED")))
+(evil-define-key 'normal org-mode-map (kbd "W")
+                 (lambda () (interactive) (org-todo "WAIT")))
+(evil-define-key 'normal org-mode-map (kbd "S")
+                 (lambda () (interactive) (org-todo "STOP")))
 (evil-define-key '(normal visual) org-mode-map (kbd "R") 'org-refile)
 (evil-define-key 'normal org-mode-map (kbd "<leader>c")
                  (lambda () (interactive) (org-capture nil "t")))
 (evil-define-key 'normal org-mode-map (kbd "<tab>") #'org-cycle)
 (evil-define-key 'normal org-mode-map (kbd "t") 'org-insert-todo-heading)
-(evil-define-key 'normal org-mode-map (kbd "T") 'org-insert-todo-subheading)
 (evil-define-key 'normal org-mode-map (kbd "gd") 'org-open-at-point)
 
 (evil-define-key '(normal visual) org-mode-map (kbd "<f5>") 'org-clock-in)
@@ -69,59 +74,85 @@
 ;; Config org-todo list
 ;; -----------------------------------------------------------------------------
 (setq org-todo-keywords
-      (quote ((sequence "TODO(t!)" "DOING(g)" "|" "CANCELLED(c)" "DONE(d)"))))
+  '((sequence
+     "TODO(t)" ; doing later
+     "NEXT(n)" ; doing now or soon
+     "|"
+     "DONE(d)" ; done
+     )
+    (sequence
+     "WAIT(w)" ; waiting for some external change (event)
+     "HOLD(h)" ; waiting for some internal change (of mind)
+     "|"
+     "STOP(s@/!)" ; stopped waiting, decided not to work on it
+     )))
 
+; Auto change from NEXT to TODO if the task are now a project and not a task.
+; (i.e. make sure NEXT is for a task and not a project)
+(defun mark-next-parent-tasks-todo ()
+  "Visit each parent task and change NEXT states to TODO"
+  (let ((mystate (or (and (fboundp 'org-state)
+                          state)
+                     (nth 2 (org-heading-components)))))
+    (when mystate
+      (save-excursion
+        (while (org-up-heading-safe)
+          (when (member (nth 2 (org-heading-components)) (list "NEXT"))
+            (org-todo "TODO")))))))
+
+(add-hook 'org-after-todo-state-change-hook 'mark-next-parent-tasks-todo 'append)
+(add-hook 'org-clock-in-hook 'mark-next-parent-tasks-todo 'append)
 ;; Automatically change to DONE when all children are done
 ;; Code from:https://christiantietze.de/posts/2021/02/emacs-org-todo-doing-done-checkbox-cycling/
-(defun org-todo-if-needed (state)
-  "Change header state to STATE unless the current item is in STATE already."
-  (unless (string-equal (org-get-todo-state) state)
-    (org-todo state)))
+; (defun org-todo-if-needed (state)
+;   "Change header state to STATE unless the current item is in STATE already."
+;   (unless (string-equal (org-get-todo-state) state)
+;     (org-todo state)))
 
-(defun ct/org-summary-todo-cookie (n-done n-not-done)
-  "Switch header state to DONE when all subentries are DONE, to TODO when none are DONE, and to DOING otherwise"
-  (let (org-log-done org-log-states)   ; turn off logging
-    (org-todo-if-needed (cond ((= n-done 0)
-                               "TODO")
-                              ((= n-not-done 0)
-                               "DONE")
-                              (t
-                               "DOING")))))
-(add-hook 'org-after-todo-statistics-hook #'ct/org-summary-todo-cookie)
+; (defun ct/org-summary-todo-cookie (n-done n-not-done)
+;   "Switch header state to DONE when all subentries are DONE, to TODO when none are DONE, and to DOING otherwise"
+;   (let (org-log-done org-log-states)   ; turn off logging
+;     (org-todo-if-needed (cond ((= n-done 0)
+;                                "TODO")
+;                               ((= n-not-done 0)
+;                                "DONE")
+;                               (t
+;                                "DOING")))))
+; (add-hook 'org-after-todo-statistics-hook #'ct/org-summary-todo-cookie)
 
-(defun ct/org-summary-checkbox-cookie ()
-  "Switch header state to DONE when all checkboxes are ticked, to TODO when none are ticked, and to DOING otherwise"
-  (let (beg end)
-    (unless (not (org-get-todo-state))
-      (save-excursion
-        (org-back-to-heading t)
-        (setq beg (point))
-        (end-of-line)
-        (setq end (point))
-        (goto-char beg)
-        ;; Regex group 1: %-based cookie
-        ;; Regex group 2 and 3: x/y cookie
-        (if (re-search-forward "\\[\\([0-9]*%\\)\\]\\|\\[\\([0-9]*\\)/\\([0-9]*\\)\\]"
-                               end t)
-            (if (match-end 1)
-                ;; [xx%] cookie support
-                (cond ((equal (match-string 1) "100%")
-                       (org-todo-if-needed "DONE"))
-                      ((equal (match-string 1) "0%")
-                       (org-todo-if-needed "TODO"))
-                      (t
-                       (org-todo-if-needed "DOING")))
-              ;; [x/y] cookie support
-              (if (> (match-end 2) (match-beginning 2)) ; = if not empty
-                  (cond ((equal (match-string 2) (match-string 3))
-                         (org-todo-if-needed "DONE"))
-                        ((or (equal (string-trim (match-string 2)) "")
-                             (equal (match-string 2) "0"))
-                         (org-todo-if-needed "TODO"))
-                        (t
-                         (org-todo-if-needed "DOING")))
-                (org-todo-if-needed "DOING"))))))))
-(add-hook 'org-checkbox-statistics-hook #'ct/org-summary-checkbox-cookie)
+; (defun ct/org-summary-checkbox-cookie ()
+;   "Switch header state to DONE when all checkboxes are ticked, to TODO when none are ticked, and to DOING otherwise"
+;   (let (beg end)
+;     (unless (not (org-get-todo-state))
+;       (save-excursion
+;         (org-back-to-heading t)
+;         (setq beg (point))
+;         (end-of-line)
+;         (setq end (point))
+;         (goto-char beg)
+;         ;; Regex group 1: %-based cookie
+;         ;; Regex group 2 and 3: x/y cookie
+;         (if (re-search-forward "\\[\\([0-9]*%\\)\\]\\|\\[\\([0-9]*\\)/\\([0-9]*\\)\\]"
+;                                end t)
+;             (if (match-end 1)
+;                 ;; [xx%] cookie support
+;                 (cond ((equal (match-string 1) "100%")
+;                        (org-todo-if-needed "DONE"))
+;                       ((equal (match-string 1) "0%")
+;                        (org-todo-if-needed "TODO"))
+;                       (t
+;                        (org-todo-if-needed "DOING")))
+;               ;; [x/y] cookie support
+;               (if (> (match-end 2) (match-beginning 2)) ; = if not empty
+;                   (cond ((equal (match-string 2) (match-string 3))
+;                          (org-todo-if-needed "DONE"))
+;                         ((or (equal (string-trim (match-string 2)) "")
+;                              (equal (match-string 2) "0"))
+;                          (org-todo-if-needed "TODO"))
+;                         (t
+;                          (org-todo-if-needed "DOING")))
+;                 (org-todo-if-needed "DOING"))))))))
+; (add-hook 'org-checkbox-statistics-hook #'ct/org-summary-checkbox-cookie)
 
 ;; -----------------------------------------------------------------------------
 ; Config refile
